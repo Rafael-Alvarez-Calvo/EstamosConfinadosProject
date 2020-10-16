572e4f5a4cc787e73babc9ff9366ffbf7e0fab8e
[LuisVegas88] <[luisvegasmenchero@gmail.com]>
2020-10-16 12:02:01 +0200

    mapa

---
+++ main.js
@@ -0,0 +1,18 @@
+let map = L.map('map', {});
+L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    maxZoom: 20
+}).addTo(map);
+var browserLat;
+var browserLong;  
+navigator.geolocation.getCurrentPosition(function(position) {
+browserLat =  position.coords.latitude;
+browserLong = position.coords.longitude;
+marker_actual = L.marker([browserLat,browserLong]).addTo(map);
+marker_actual.bindPopup('<b>Hola </b><br>Tu estas aqui').openPopup();
+map.setView([browserLat,browserLong], 18);  
+console.log(browserLat);
+console.log(browserLong); }, 
+function(err) {
+console.error(err);
+});   
