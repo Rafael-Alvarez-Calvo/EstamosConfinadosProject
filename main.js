8cf85fb9785fe8fc78dffb4aa3044226749ba5fe
[LuisVegas88] <[luisvegasmenchero@gmail.com]>
2020-10-21 16:43:06 +0200

    Esto ya va

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ class Incidencia {
       ul3li11.innerText =
         "Cualquier otra actividad de análoga naturaleza, debidamente acreditada.";
       ul3li12.innerText =
-        "Quedarse en casa es mas barato. Por la HordAAAAAAAAAAAAAA";
+        "Quedarse en casa es mas barato.";
       confinamiento.appendChild(h1);
       confinamiento.appendChild(parrafo1);
       confinamiento.appendChild(parrafo2);
@@ -158,7 +158,7 @@ async function datos() {
   if (lastfind === undefined) {
     respuesta = await  fetch(
       "https://apifetcher.herokuapp.com/?id=f22c3f43-c5d0-41a4-96dc-719214d56968&filters=" +
-      JSON.stringify({ municipio_distrito: "Madrid-"+ "Centro"/*coordenadas*/})   
+      JSON.stringify({ municipio_distrito: "Madrid-"+ await Coordenadas(browserLat, browserLong)})   
     )
     resultado = await respuesta.json()
      resultado = {datos : "API",...resultado}
@@ -178,7 +178,7 @@ async function datos() {
 ////////////////////////// MAPA /////////////////////////////////////////////////////////////
 function mapa() {
   L.tileLayer(
-    "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png",
+    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
     {
       attribution:
         '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
@@ -212,7 +212,7 @@ async function Coordenadas (browserLat,browserLong){
   let data = await response.json();
     // console.log(data);
     // console.log(data.address.city_district);
-    return /* data.address.city_district*/ "Madrid-Centro"
+    return data.address.city_district
 }
 
 
